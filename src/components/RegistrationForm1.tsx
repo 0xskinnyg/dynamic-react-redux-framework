5393a2f564fc7376c75bd3837a70a49e67415021
r1ck12 <0xr1ck12@gmail.com>
2024-08-07 13:35:47 -0300

    feat: add forms

--- src/components/RegistrationForm1.tsx
+++ src/components/RegistrationForm1.tsx
@@ -1,7 +1,34 @@
-import React from 'react';
+import React from "react";
+import { Button, ButtonToolbar, Form, Input, Text } from "rsuite";
 
 const RegistrationForm1: React.FC = () => {
-  return <div>This is Registration Form Version 1</div>;
+  return (
+    <div>
+      <Form>
+        <Form.Group controlId="name">
+          <Form.ControlLabel>Username</Form.ControlLabel>
+          <Form.Control name="name" />
+          <Form.HelpText>Username is required</Form.HelpText>
+        </Form.Group>
+        <Form.Group controlId="email">
+          <Form.ControlLabel>Email</Form.ControlLabel>
+          <Form.Control name="email" type="email" />
+          <Form.HelpText tooltip>Email is required</Form.HelpText>
+        </Form.Group>
+        <Form.Group controlId="password">
+          <Form.ControlLabel>Password</Form.ControlLabel>
+          <Form.Control name="password" type="password" autoComplete="off" />
+        </Form.Group>
+        <Form.Group>
+          <ButtonToolbar>
+            <Button appearance="primary">Submit</Button>
+            <Button appearance="default">Cancel</Button>
+          </ButtonToolbar>
+        </Form.Group>
+      </Form>
+      <Text>This is Registration Form Version 1</Text>
+    </div>
+  );
 };
 
 export default RegistrationForm1;
--- src/components/RegistrationForm2.tsx
+++ src/components/RegistrationForm2.tsx
@@ -1,7 +1,34 @@
-import React from 'react';
+import React from "react";
+import { Form, ButtonToolbar, Button, Text } from "rsuite";
 
 const RegistrationForm2: React.FC = () => {
-  return <div>This is Registration Form Version 2</div>;
+  return (
+    <div>
+      <Form>
+        <Form.Group controlId="name">
+          <Form.ControlLabel>Username</Form.ControlLabel>
+          <Form.Control name="name" />
+          <Form.HelpText>Username is required</Form.HelpText>
+        </Form.Group>
+        <Form.Group controlId="phone">
+          <Form.ControlLabel>Phone</Form.ControlLabel>
+          <Form.Control name="Phone" type="Phone" />
+          <Form.HelpText tooltip>Phone is required</Form.HelpText>
+        </Form.Group>
+        <Form.Group controlId="password">
+          <Form.ControlLabel>Password</Form.ControlLabel>
+          <Form.Control name="password" type="password" autoComplete="off" />
+        </Form.Group>
+        <Form.Group>
+          <ButtonToolbar>
+            <Button appearance="primary">Submit</Button>
+            <Button appearance="default">Cancel</Button>
+          </ButtonToolbar>
+        </Form.Group>
+      </Form>
+      <Text>This is Registration Form Version 2</Text>
+    </div>
+  );
 };
 
 export default RegistrationForm2;
